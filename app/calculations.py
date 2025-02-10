98de3f26ae9fa68e14b8418b8ee18eae863dba55
faruk.tuefeki@gmail.com <faruktuefekli@Mac-Pro.local>
2025-02-10 16:13:33 +0100

    neue Seite ProfilCalc19

--- app/calculations.py
+++ app/calculations.py
@@ -33,7 +33,7 @@ def calculate_verpressungsverhaeltnis(profile: StrangpressprofilSchema, anlage: 
 def calculate_max_theor_profgesch(profile: StrangpressprofilSchema, anlage: ExtrusionsanlageSchema) -> float:
     validate_inputs(profile, anlage)
     verpressungsverhaeltnis = calculate_verpressungsverhaeltnis(profile, anlage)
-    max_theor_profgesch_mm_s = (anlage.max_stempelgeschwindigkeit / verpressungsverhaeltnis) * 60 / 1000  # Umrechnung von m/min in mm/s
+    max_theor_profgesch_mm_s = (anlage.max_stempelgeschwindigkeit / verpressungsverhaeltnis) / 60 * 1000  # Umrechnung von m/min in mm/s
 
     return round(max(1.0, max_theor_profgesch_mm_s), 2)
 
